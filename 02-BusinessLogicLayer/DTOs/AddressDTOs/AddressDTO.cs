6635f62c65934fb2d7cad4bbdb4d50707eb2e545
sayedAli <said.ali199859@gmail.com>
2025-07-19 18:27:52 +0300

    added address Service and updated controller

---
+++ 02-BusinessLogicLayer/DTOs/AddressDTOs/AddressDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace _02_BusinessLogicLayer.DTOs.AddressDTOs
+{
+    public class AddressDTO
+    {
+        public string Location { get; set; }
+        public string DetailedAddress { get; set; }
+
+        public int CityId { get; set; }
+        public int DoctorId { get; set; }
+    }
+}
