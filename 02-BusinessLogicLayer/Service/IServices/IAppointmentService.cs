2f84f80942c558465d642818c93f56b9b76d8001
sayedAli <said.ali199859@gmail.com>
2025-07-21 21:40:31 +0300

    added appointement service , controller

---
+++ 02-BusinessLogicLayer/Service/IServices/IAppointmentService.cs
@@ -0,0 +1,23 @@
+﻿using _01_DataAccessLayer.Models;
+using _02_BusinessLogicLayer.DTOs.AppointmentDTOs;
+using System.Linq.Expressions;
+
+namespace _02_BusinessLogicLayer.Service.IServices
+{
+    public interface IAppointmentService
+    {
+        Task<AppointmentDTO> AddAppointmentAsync(AppointmentDTO appointmentDto);
+        Task<AppointmentDTO> GetAppointmentByIdAsync(int appointmentId);
+
+        Task<bool> ConfirmAppointmentAsync(int appointmentId);
+        Task<IEnumerable<AppointmentDTO>> GetAllAppointmentsAsync();
+
+        Task<bool> UpdateAppointmentAsync(AppointmentDTO appointment, int appointmentId);
+
+        Task<bool> DeleteAppointmentByIdAsync(int appointmentId);
+        Task<bool> DeleteAppointmentAsync(AppointmentDTO appointmentDTO, int id);
+
+        Task<int> CountAsync(Expression<Func<Appointment, bool>>? filter = null);
+        Task<bool> ExistsAsync(Expression<Func<Appointment, bool>> predicate);
+    }
+}
