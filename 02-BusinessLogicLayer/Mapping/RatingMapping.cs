3a4e213005584cb47e090164ec21185a0b83df5b
Ali Gamal <aligamaldakhly99@gmail.com>
2025-07-27 20:45:41 +0300

    Updated Mapping Classes.

---
+++ 02-BusinessLogicLayer/Mapping/RatingMapping.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _01_DataAccessLayer.Models;
+using _02_BusinessLogicLayer.DTOs.PatientDTOs;
+using _02_BusinessLogicLayer.DTOs.RatingDTOs;
+using AutoMapper;
+
+namespace _02_BusinessLogicLayer.Mapping
+{
+    public class RatingMapping: Profile
+    {
+        public RatingMapping() {
+
+
+            CreateMap<Rating, RatingDTO>()
+                .ForMember(dest => dest.RatingValue, opt => opt.MapFrom(src => src.RatingValue))
+                .ForMember(dest => dest.Comment, opt => opt.MapFrom(src => src.Comment))
+                .ForMember(dest => dest.DoctorId, opt => opt.MapFrom(src => src.DoctorId))
+                .ForMember(dest => dest.PatientId, opt => opt.MapFrom(src => src.PatientId));
+
+            CreateMap<RatingDTO, Rating>()
+                .ForMember(dest => dest.RatingValue, opt => opt.MapFrom(src => src.RatingValue))
+                .ForMember(dest => dest.Comment, opt => opt.MapFrom(src => src.Comment))
+                .ForMember(dest => dest.DoctorId, opt => opt.MapFrom(src => src.DoctorId))
+                .ForMember(dest => dest.PatientId, opt => opt.MapFrom(src => src.PatientId));
+
+            // Rating
+            CreateMap<AddRatingDTO, Rating>();
+            CreateMap<UpdateRatingDTO, Rating>();
+        }
+    }
+}
--- 02-BusinessLogicLayer/Mapping/TimeSlotMapping.cs
+++ 02-BusinessLogicLayer/Mapping/TimeSlotMapping.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _01_DataAccessLayer.Enums;
 using _01_DataAccessLayer.Models;
 using _02_BusinessLogicLayer.DTOs.DoctorTimeSlotDTOs;
+using _02_BusinessLogicLayer.DTOs.TimeSlotDTOs;
 using AutoMapper;
 
 namespace _02_BusinessLogicLayer.Mapping
@@ -19,6 +21,19 @@ namespace _02_BusinessLogicLayer.Mapping
                 .ForMember(dest => dest.EndTime, opt => opt.MapFrom(src => src.TimeSlot.EndTime))
                 .ForMember(dest => dest.DoctorId, opt => opt.MapFrom(src => src.DoctorId))
                 .ForMember(dest => dest.TimeSlotId, opt => opt.MapFrom(src => src.TimeSlotId));
+
+
+            //for view
+            CreateMap<TimeSlot, TimeSlotDTO>()
+              .ForMember(dest => dest.DayOfWeek, opt => opt.MapFrom(src => src.DayOfWeek.ToString()))
+              .ReverseMap()
+             .ForMember(dest => dest.DayOfWeek, opt => opt.MapFrom(src => Enum.Parse<WeekDays>(src.DayOfWeek)));
+
+            //for edit
+            CreateMap<TimeSlot, EditTimeSlotDTO>().ReverseMap();
+
+            //for create will not pass id
+            CreateMap<TimeSlot, CreateTimeSlotDTO>().ReverseMap();
         }
     }
 }
