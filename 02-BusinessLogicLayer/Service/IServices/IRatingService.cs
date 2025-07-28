0e89a8bf742e27a086631e42a11b85698e672552
Ali Gamal <aligamaldakhly99@gmail.com>
2025-07-28 05:22:57 +0300

    updated Rating Service , added GetAllDoctorRatings.

--- 02-BusinessLogicLayer/Service/IServices/IRatingService.cs
+++ 02-BusinessLogicLayer/Service/IServices/IRatingService.cs
@@ -21,5 +21,6 @@ namespace _02_BusinessLogicLayer.Service.IServices
         Task<int> CountAsync(Expression<Func<Rating, bool>>? filter = null);
         Task<bool> ExistsAsync(Expression<Func<Rating, bool>> predicate);
         Task<List<RatingDTO>> GetAllAsync(QueryOptions<Rating>? options = null);
+        Task<List<DoctorRatingDTO>> GetAllDoctorRatings(int doctorId);
     }
 }
