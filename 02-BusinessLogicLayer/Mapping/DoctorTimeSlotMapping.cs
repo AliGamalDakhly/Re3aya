561560092dbd7aac27590ea5b5659ad72a636d4c
Ali Gamal <aligamaldakhly99@gmail.com>
2025-07-29 17:02:49 +0300

    Updated Mapping.

---
+++ 02-BusinessLogicLayer/Mapping/DoctorTimeSlotMapping.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _01_DataAccessLayer.Models;
+using _02_BusinessLogicLayer.DTOs.DoctorDTOs;
+using _02_BusinessLogicLayer.DTOs.DoctorTimeSlotDTOs;
+using AutoMapper;
+
+namespace _02_BusinessLogicLayer.Mapping
+{
+    public class DoctorTimeSlotMapping: Profile
+    {
+        public DoctorTimeSlotMapping() {
+
+            CreateMap<AddDoctorTimeSlotDTO, DoctorTimeSlot>()
+                .ForMember(dest => dest.DoctorTimeSlotId, opt => opt.MapFrom(src => src.DoctorTimeSlotId))
+                .ForMember(dest => dest.DoctorId, opt => opt.MapFrom(src => src.DoctorId))
+                .ForMember(dest => dest.TimeSlotId, opt => opt.MapFrom(src => src.TimeSlotId))
+                .ForMember(dest => dest.IsAvailable, opt => opt.MapFrom(src => src.IsAvailable));
+
+            CreateMap<DoctorTimeSlot, AddDoctorTimeSlotDTO>()
+                .ForMember(dest => dest.DoctorTimeSlotId, opt => opt.MapFrom(src => src.DoctorTimeSlotId))
+                .ForMember(dest => dest.DoctorId, opt => opt.MapFrom(src => src.DoctorId))
+                .ForMember(dest => dest.TimeSlotId, opt => opt.MapFrom(src => src.TimeSlotId))
+                .ForMember(dest => dest.IsAvailable, opt => opt.MapFrom(src => src.IsAvailable));
+
+        }
+    }
+}
