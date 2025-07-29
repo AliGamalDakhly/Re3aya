c0f78519d6101b600f0ce16a7167ff9548dc43de
Ali Gamal <aligamaldakhly99@gmail.com>
2025-07-29 17:04:41 +0300

    Added Imore function here.

--- 02-BusinessLogicLayer/Service/IServices/IDoctorTimeSlotService.cs
+++ 02-BusinessLogicLayer/Service/IServices/IDoctorTimeSlotService.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _01_DataAccessLayer.Models;
+using _01_DataAccessLayer.Repository;
 using _02_BusinessLogicLayer.DTOs.DoctorTimeSlotDTOs;
 
 namespace _02_BusinessLogicLayer.Service.IServices
 {
     public interface IDoctorTimeSlotService
     {
+        Task<AddDoctorTimeSlotDTO> AddDoctorTimeSlot(AddDoctorTimeSlotDTO doctorTimeSlotDTO);
         Task<List<AvailableDoctorTImeSlotDTO>> AvailableDoctorTimeSlots(int doctorId, DateOnly date);
-
+        Task<List<AddDoctorTimeSlotDTO>> GetAllAsync(QueryOptions<DoctorTimeSlot>? options = null);
     }
 }
