6c08a4c5daa149d5d4503abbecf4454c1a00aa5b
Abdullah Moqreb <abdallah.mokarb@gmail.com>
2025-07-31 16:19:06 +0300

    updated payment endpoints apis by am

---
+++ 02-BusinessLogicLayer/DTOs/DoctorTimeSlotDTOs/AddDoctorTimeSlotDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_BusinessLogicLayer.DTOs.DoctorTimeSlotDTOs
+{
+    public class AddDoctorTimeSlotDTO
+    {
+        public int DoctorTimeSlotId { get; set; }
+        public int DoctorId { get; set; }
+        public int TimeSlotId { get; set; }
+
+        public bool IsAvailable = true;
+    }
+}
