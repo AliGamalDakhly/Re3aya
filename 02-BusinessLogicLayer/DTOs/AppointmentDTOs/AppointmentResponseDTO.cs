235d4c3f1a0787d17aea8ba24e570ff977ed7295
Abdullah Moqreb <abdallah.mokarb@gmail.com>
2025-08-07 19:01:42 +0300

    added new endpoint to get all doctors appointments with its payments also for patient and more by am

--- 02-BusinessLogicLayer/DTOs/AppointmentDTOs/AppointmentResponseDTO.cs
+++ 02-BusinessLogicLayer/DTOs/AppointmentDTOs/AppointmentResponseDTO.cs
@@ -1,25 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace _02_BusinessLogicLayer.DTOs.PatientDTOs
+namespace _02_BusinessLogicLayer.DTOs
 {
-    public class AppointmentWithPatientDTO
+    public class AppointmentResponseDTO
     {
         public int AppointmentId { get; set; }
 
         public string DoctorName { get; set; }
+        //public string PatientName { get; set; }
         public string SpecializationName { get; set; }
+
         public string StartTime { get; set; }
         public string EndTime { get; set; }
+
         public DateTime Date { get; set; }
         public string? VedioCallUrl { get; set; }
         public string? Notes { get; set; }
         public string Status { get; set; }
-        public int PatientId { get; set; }
-        public string PatientName { get; set; }
-        public int PaymentId { get; set; }
-        public string TransactionId { get; set; }
-        public double Amount { get; set; }
-
-
     }
 }
