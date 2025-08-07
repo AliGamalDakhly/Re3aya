99f12fcd44593e94ec44f53095e7ed43d7eefe38
Ali Gamal <aligamaldakhly99@gmail.com>
2025-08-08 01:57:09 +0300

    added system info functionalities.

---
+++ 02-BusinessLogicLayer/Service/IServices/ISystemInfoService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_BusinessLogicLayer.Service.IServices
+{
+    public interface ISystemInfoService
+    {
+        Task<double> GetBalance();
+        Task<bool> UpdateBalance(double balance);
+        Task<bool> IncrementBalance(double amount);
+        Task<bool> DecrementBalance(double amount);
+        Task<double> CalcBalance();
+    }
+}
---
+++ 02-BusinessLogicLayer/Service/Services/SystemInfoService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _01_DataAccessLayer.Enums;
+using _01_DataAccessLayer.Models;
+using _01_DataAccessLayer.Repository;
+using _01_DataAccessLayer.Repository.IGenericRepository;
+using _01_DataAccessLayer.UnitOfWork;
+using _02_BusinessLogicLayer.Service.IServices;
+using AutoMapper;
+
+namespace _02_BusinessLogicLayer.Service.Services
+{
+    public class SystemInfoService : ISystemInfoService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericRepository<SystemInfo, int> _context;
+        private readonly IMapper _mapper;
+
+        public SystemInfoService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _context = _unitOfWork.Repository<SystemInfo, int>();
+            _mapper = mapper;
+        }
+
+        public async Task<double> GetBalance()
+        {
+            SystemInfo systemInfo = await _context.GetByIdAsync(2);
+            if (systemInfo == null)
+                throw new Exception("System information not found.");
+            return systemInfo.Balance;
+        }
+
+        public async Task<bool> UpdateBalance(double balance)
+        {
+            SystemInfo systemInfo = await _context.GetByIdAsync(2);
+            if (systemInfo == null)
+                throw new Exception("System information not found.");
+            systemInfo.Balance = balance;
+
+            await _unitOfWork.CompleteAsync();
+            return true;
+        }
+
+        public async Task<bool> IncrementBalance(double amount)
+        {
+            SystemInfo systemInfo = await _context.GetByIdAsync(2);
+            if (systemInfo == null)
+                throw new Exception("System information not found.");
+            systemInfo.Balance += amount;
+
+            await _unitOfWork.CompleteAsync();
+            return true;
+        }
+
+        public async Task<bool> DecrementBalance(double amount)
+        {
+            SystemInfo systemInfo = await _context.GetByIdAsync(2);
+            if (systemInfo == null)
+                throw new Exception("System information not found.");
+            systemInfo.Balance -= amount;
+
+            await _unitOfWork.CompleteAsync();
+            return true;
+        }
+
+        public async Task<double> CalcBalance()
+        {
+            List<Doctor> doctors = await _unitOfWork.Repository<Doctor, int>().GetAllAsync();
+
+            double totalBalance = 0;
+
+            foreach (var doctor in doctors)
+            {
+                totalBalance += (.1 * (doctor.Balance / .9));
+            }
+
+            SystemInfo systemInfo = await _context.GetByIdAsync(2);
+            if (systemInfo == null)
+                throw new Exception("System information not found.");
+
+            systemInfo.Balance = totalBalance;
+            await _unitOfWork.CompleteAsync();
+            return systemInfo.Balance;
+        }
+    }
+}
